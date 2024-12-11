3509843c9389acb07feeb5954122a169162bd843
YSLennon <ysha0123@naver.com>
2024-12-12 03:38:29 +0900

    to War

--- bagStrap/src/main/java/com/example/bagStrap/BagStrapApplication.java
+++ bagStrap/src/main/java/com/example/bagStrap/BagStrapApplication.java
@@ -3,6 +3,8 @@ package com.example.bagStrap;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +12,12 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("com.example.bagStrap.mapper") // ItemMapper가 위치한 패키지
-public class BagStrapApplication {
+public class BagStrapApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BagStrapApplication.class);
+    }
 
     @Bean
     public RestTemplate restTemplate() {
